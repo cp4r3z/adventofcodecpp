48c8acae44259468230c54d55c1a6bc2ba25d550
cp4r3z <cp4r3z@gmail.com>
2017-12-14 03:34:08 +0000

    Ok, basic map stolen from the internet. Baby steps.

--- hello-cpp-world.cc
+++ hello-cpp-world.cc
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <map>
 using namespace std;
+
+bool fncomp (char lhs, char rhs) {return lhs<rhs;}
+
+struct classcomp {
+    bool operator() (const char& lhs, const char& rhs) const
+    {return lhs<rhs;}
+};
+
 
 int main() {
     cout << "Hello World!" << endl;
+    
+    std::map<char,int> first;
+
+    first['a']=10;
+    first['b']=30;
+    first['c']=50;
+    first['d']=70;
+
+    map<char,int> second (first.begin(),first.end());
+
+    map<char,int> third (second);
+
+    map<char,int,classcomp> fourth;                 // class as Compare
+
+    bool(*fn_pt)(char,char) = fncomp;
+    map <char,int,bool(*)(char,char)> fifth (fn_pt); // function pointer as Compare
+
+    return 0;
+    
 }
